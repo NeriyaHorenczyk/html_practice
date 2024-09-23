498324175e297aaa748ea9a97d8244f391e83595
neriya <ner13579@gmail.com>
2024-09-23 22:41:47 +0300

    learned some basic JS commands and methods

---
+++ season2/lesson1/index.js
@@ -0,0 +1,23 @@
+// console.log(`Hello`)
+// console.log(`I Love You!`)
+
+// window.alert(`ALARM!!`)
+
+// document.getElementById("myH1").textContent="Hello";
+// document.getElementById("myParag").textContent="I Love You";
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 3.1;
+
+// console.log(`the type of age is ` + typeof age)
+// console.log(`You are ${age} years old`);
+// console.log(`The price is ${price} NIS`);
+
+let fullName = "Nerserjon";
+let age = 26;
+let student = true;
+
+document.getElementById("p1").textContent = `My name is ${fullName}`;
+document.getElementById("p2").textContent = `I am ${age} years old`;
+document.getElementById("p3").textContent = `Am I currently enrolled? ${student}`;
